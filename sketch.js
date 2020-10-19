7cfcbcc19fa11bd2d4961640025d0e03fb9ccbc0
pvsslr <vssunil1997@gmail.com>
2020-10-19 16:03:37 +0530

    t

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+var circleX = 10;
+var CircleY = 10;
 function setup() {
   createCanvas(1920, 1080);
   background(119, 119, 119);
@@ -7,19 +9,15 @@ function draw() {
   
   fill(255,0,0);
   //circle(100,mouseX,50);
-  circle(mouseY,500 - mouseY,50);
-  circle(mouseY,mouseX,50);
-  circle(mouseX,mouseY,50);
-  circle(mouseY+100,mouseX,50);
-  circle(mouseX,mouseY+300,50);
-  
+  circle(CircleY,500 - CircleY,50);
+  circle(CircleY,circleX,50);
+  circle(circleX,CircleY,50);
+  circle(CircleY+100,circleX,50);
+  circle(circleX,CircleY+300,50);
+  circleX = circleX + 5; 
+  CircleY = CircleY + 5;
 }
 
 function mousePressed() {
    background(119, 119, 119);
-  circle(mouseY,500 - mouseY,50);
-  circle(mouseY,mouseX,50);
-  circle(mouseX,mouseY,50);
-  circle(mouseY+100,mouseX,50);
-  circle(mouseX,mouseY+300,50);
 }
